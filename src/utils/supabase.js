2f614e36121885d378d25789d4e61b913ac59d64
aquibbaig <aquibbaig97@gmail.com>
2022-03-07 16:01:54 +0530

    update supabase api key namespace

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_APP || "";
-const supabaseServerKey = process.env.SUPABASE_SECRET || "";
+const supabaseServerKey = process.env.NEXT_PUBLIC_SUPABASE_SECRET || "";
 
 export default function useSupabase() {
   const supabaseClient = createClient(supabaseUrl, supabaseServerKey);
